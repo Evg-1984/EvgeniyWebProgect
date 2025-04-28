69e347ff4cd6f13d5108e5fdfba08e6ce81008c3
Evg-1984 <evgeniy120809@gmail.com>
2025-04-28 20:54:11 +0700

    ver 0.0.5

--- server.py
+++ server.py
@@ -44,9 +44,6 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Войти')
 
 
-session.pop('visits_count', None)
-
-
 class AudioResource(Resource):
     def get(self, file_id):
         abort_if_audiofile_not_found(file_id)
@@ -120,9 +117,20 @@ def session_test():
         f"Вы пришли на эту страницу {visits_count + 1} раз")
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
 if __name__ == '__main__':
-    # для списка объектов
-    api.add_resource(news_resources.AudioListResource, '/api/v2/news')
-    # для одного объекта
-    api.add_resource(audio_resources.AudioResource, '/api/v2/news/<int:news_id>')
     main()
