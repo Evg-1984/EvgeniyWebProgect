daeedffdeefd37791c058ff0aeeefbeb66048f81
Evg-1984 <evgeniy120809@gmail.com>
2025-05-03 23:53:03 +0700

    ver 0.1.6

--- server.py
+++ server.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, make_response, request, session
 from data import db_session
 from data.users import User
 from data.music import Audio
-from forms.user import RegisterForm
+from forms.user import RegisterForm, LoginForm
 import datetime
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user
 
 
 app = Flask(__name__)
@@ -14,6 +14,21 @@ app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 @login_manager.user_loader
